26e18ca9599b908770355a4459f3e5ffc0b7e969
Ayushi Patel <ashapatel99246@gmail.com>
2025-04-19 11:05:15 +0530

    admin header solved

--- frontend/src/pages/admin/Header.jsx
+++ frontend/src/pages/admin/Header.jsx
@@ -9,26 +9,6 @@ const Header = () => {
         <p className="text-[#759B87]">Welcome back, Admin</p>
       </div>
 
-      {/* Right Section - Search & Notifications */}
-      <div className="flex items-center gap-6">
-        {/* Search Bar */}
-        <div className="relative">
-          <Search className="w-5 h-5 absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-          <input
-            type="text"
-            placeholder="Search..."
-            className="pl-10 pr-4 py-2 rounded-lg border border-gray-200 focus:outline-none focus:border-[#D8B258]"
-          />
-        </div>
-
-        {/* Notification Bell */}
-        <button className="relative">
-          <Bell className="w-6 h-6 text-[#103538]" />
-          <span className="absolute -top-1 -right-1 w-4 h-4 bg-[#D96851] rounded-full text-white text-xs flex items-center justify-center">
-            3
-          </span>
-        </button>
-      </div>
     </div>
   );
 };
